0883293587f42744d9f680a021757470c96d3829
EJvdL <ejvanderlinden@gmail.com>
2023-07-06 14:54:51 +0200

    Working version
    Logs two temperature sensor values to a ThingSpeak channel

--- thingSpeakTemperature.ino
+++ thingSpeakTemperature.ino
@@ -27,57 +27,171 @@
  Tutorial: http://nothans.com/measure-wi-fi-signal-levels-with-the-esp8266-and-thingspeak
    
  Created: Feb 3, 2017 by Hans Scharler (http://nothans.com)
+
+  To connect the Dallas thermometers
+  https://lastminuteengineers.com/multiple-ds18b20-arduino-tutorial/
+ 
 */
+
+//#define DEBUG
 
 #include "ThingSpeak.h"
 #include "MySecrets.h"
+#include <ESP8266WiFi.h>
+
+#include <OneWire.h> 
+#include <DallasTemperature.h>
 
 unsigned long myChannelNumber = SECRET_CH_ID;
-const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+const    char * myWriteAPIKey = SECRET_WRITE_APIKEY;
 
-#include <ESP8266WiFi.h>
 
 char ssid[] = SECRET_SSID;   // your network SSID (name)
 char pass[] = SECRET_PASS;   // your network password
 int keyIndex = 0;            // your network key index number (needed only for WEP)
 WiFiClient  client;
 
+const char* statuses[] =  { "WL_IDLE_STATUS=0", "WL_NO_SSID_AVAIL=1", "WL_SCAN_COMPLETED=2", "WL_CONNECTED=3", "WL_CONNECT_FAILED=4", "WL_CONNECTION_LOST=5", "WL_DISCONNECTED=6"};
+
+#define ONE_WIRE_BUS 4            // Data wire is plugged into pin 4 = D2 on the Arduino 
+OneWire           mvOneWire(ONE_WIRE_BUS); 
+DallasTemperature mvSensors(&mvOneWire);
+
+/* -----
+ * wifiReconnect
+ * Checks the current connection state of the WiFi.
+ * If not connected, try to re-connect.
+ * -----
+ */
+void wifiReconnect() {
+  if (WiFi.status() != WL_CONNECTED) {
+    #ifdef DEBUG
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(SECRET_SSID);
+    #endif
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, pass); // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      delay(5000);
+    #ifdef DEBUG
+      Serial.print(".");
+    #endif      
+    }
+    #ifdef DEBUG
+      Serial.println("\nConnected.");
+    #endif  
+  } else {
+    Serial.print("*");  //Check how often the WiFi connection is broken
+  }
+}
+
+/* -----
+ * monitorWiFi
+ * Outputs WiFi status and IP@ to the serial output
+ * -----
+ */
+void monitorWiFi()
+{
+  Serial.println();
+  Serial.print("wlstatus:");
+  Serial.println(statuses[WiFi.status()]);
+  Serial.print("WiFi.localIP():");
+  Serial.println(WiFi.localIP().toString());
+  Serial.print("IP.isSet: ");
+  Serial.println(WiFi.localIP().isSet());
+}
+
 void setup() {
+  /* -----
+   * Setup Serial
+   * -----
+   */
   Serial.begin(115200);
   delay(100);
-
+  Serial.println();
+  
+  /* -----
+   * Setup WiFi to a fixed IP address
+   * -----
+   */
   WiFi.mode(WIFI_STA);
+  
+  IPAddress ip(192,168,2,96);
+  IPAddress dns(192, 168, 2, 254);
+  IPAddress gateway(192, 168, 2, 254);
+  IPAddress subnet(255, 255, 255, 0);  
 
+  bool lvResult = WiFi.config(ip, dns, gateway, subnet);
+ 
+  #ifdef DEBUG
+    if (!lvResult) {
+      Serial.println("STA Failed to configure");
+    }
+    monitorWiFi();
+  #endif  
+  
+  /* -----
+   * Start a client to connect to the ThingSpeak framework
+   * Sensor data is going to be logged to ThingSpeak channels
+   * -----
+   */
   ThingSpeak.begin(client);
+
+  /* -----
+   * Start the communication to the Dallas temperature sensors. 
+   * Check if at least two temperature sensors are found.
+   * If no or only one sensor found stop execution but give up control to the ESP with the yield() function 
+   * -----
+   */  
+  mvSensors.begin();
+  if (mvSensors.getDeviceCount() == 0) {
+    Serial.println("Only one sensor is found, at least two sensors are expected");
+    while (true) {
+      yield();
+    }
+  }
 }
 
 void loop() {
+  float lvTemp;
+  int   lvHttpCode;
+  
+  /* -----
+   * Connect or reconnect to WiFi
+   * -----
+   */
+   wifiReconnect();
 
-  // Connect or reconnect to WiFi
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(SECRET_SSID);
-    while (WiFi.status() != WL_CONNECTED) {
-      WiFi.begin(ssid, pass); // Connect to WPA/WPA2 network. Change this line if using open or WEP network
-      Serial.print(".");
-      delay(5000);
-    }
-    Serial.println("\nConnected.");
+  /* -----
+   * Send a command on the I2C bus to all Temp sensors to take a temperature measurement
+   * -----
+   */
+  mvSensors.requestTemperatures();  
+
+  /* -----
+   * Read the degree Celcius temperature from the sensor(s) and
+   * log it to my ThingSpeak Channel on Field 1 and Field 2
+   * Note: you can only send data to ThinSpeak once each 15 seconds, so a delay 20 second is added
+   * -----
+   */
+  for (int i = 0; i <= 1; i++) {
+    lvTemp = mvSensors.getTempCByIndex(i);
+    lvHttpCode = ThingSpeak.writeField(myChannelNumber, i + 1, lvTemp, myWriteAPIKey);
+    
+    #ifdef DEBUG
+      Serial.print("ChannelNumber: ");
+      Serial.println(myChannelNumber);
+      Serial.print("Channel: ");
+      Serial.println(i + 1);
+      Serial.print("Value: ");
+      Serial.println(lvTemp);
+      if (lvHttpCode == 200) {
+          Serial.println("Channel write successful.");
+        }
+        else {
+          Serial.println("Problem writing to channel. HTTP error code " + String(lvHttpCode));
+        }
+    #endif
+
+    delay(20000);
   }
-
-  // Measure Analog Input (A0)
-  int valueA0 = analogRead(A0);
-
-  // Write value to Field 1 of a ThingSpeak Channel
-  int httpCode = ThingSpeak.writeField(myChannelNumber, 1, valueA0, myWriteAPIKey);
-
-  if (httpCode == 200) {
-    Serial.println("Channel write successful.");
-  }
-  else {
-    Serial.println("Problem writing to channel. HTTP error code " + String(httpCode));
-  }
-
-  // Wait 20 seconds to update the channel again
-  delay(20000);
 }
